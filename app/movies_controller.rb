a3ea438ba74d7b40e8703e514fcb7e32f837dbe2
Stef Claus <stef.claus@gmail.com>
2019-01-21 21:13:15 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -89,6 +89,7 @@ def can_update_multiple_items_at_once
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
   movie = Movie.find_by(title: "Movie")
+  movie.update({title: "A Movie"})
 end
 
 def can_destroy_a_single_item
